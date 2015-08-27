93ba8fba5bb2f909aa4e3aa25d33f658ee157835
Karishma Bhatia <bkrish19@gmail.com>
2015-08-26 20:08:39 -0400

    add comments

--- lib/MaxSumSubarray.cs
+++ lib/MaxSumSubarray.cs
@@ -4,24 +4,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-
-
     public class Program
     {
         public static void Main(string[] args)
         {
             //Your code goes here
+            //sample test case
             int[] B = new int[] {1,-3,5,-2,9,-8,-6,4};
-            
             int[]C = MaxSum(B);
-            
-            for(int i=0; i <C.Length; i++)
-            {
+            for(int i=0; i <C.Length; i++){
                 Console.WriteLine(C[i]);
             }
-
         }
-        
+
         public static int[] MaxSum(int[] A)
         {
             int sum = 0;
@@ -30,28 +25,22 @@ using System.Text.RegularExpressions;
             int b=0;
             int s=0;
             List<int> subList = new List<int>();
-            
-            for(int i= 0; i<A.Length; i++)
-            {
+            for(int i= 0; i<A.Length; i++){
                 sum += A[i];
-                if (sum > maxSum)
-                {
+                if (sum > maxSum){
                         maxSum = sum;
-                        a = s;
+                        a = s;  //keep record of indices
                         b = i;
-                        
                 }
-                if (sum < 0)
-                {
+                if (sum < 0){
                     sum=0;
                     s = i + 1;
-                }                                 
-            }            
-            for(int j = a; j <b+1; j++)
-            {
+                }
+            }
+            for(int j = a; j <b+1; j++){
                 subList.Add(A[j]);
             }
-            
+
             return subList.ToArray();
         }
     }
